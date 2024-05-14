8ec4f63498fa65b7154cd2a4764cb15c92e565e2
Andrew <andrew.sideby@gmail.com>
2024-05-14 15:44:13 +0800

    ok build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,57 +1,55 @@
-import { Providers } from './providers';
-import { cn } from '@/lib/utils';
-import type { Metadata } from 'next';
-import { Inter, Inter as FontSans } from 'next/font/google';
-import '../styles/globals.css';
-import {ContextProps, TMAProvider} from '@/contexts/TMA';
-import { headers } from 'next/headers';
-import { Toaster } from '@/components/ui/toaster';
-import React from 'react';
+import { Providers } from "./providers";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "../styles/globals.css";
+import { ContextProps } from "@/contexts/TMA";
+import { headers } from "next/headers";
+import { Toaster } from "@/components/ui/toaster";
+import React from "react";
 
-export const runtime = 'edge'; // 'nodejs' (default) | 'edge'
-
-const inter = Inter({ subsets: ['latin'] });
+export const runtime = "edge"; // 'nodejs' (default) | 'edge'
 
 const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: 'ART-404',
-  description: 'State of NFT Art',
+  title: "ART-404",
+  description: "State of NFT Art",
   icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
   },
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
-  const headersForContext: ContextProps['headers'] = {};
-  headers().forEach((value:any, key:any) => (headersForContext[key] = value));
+  const headersForContext: ContextProps["headers"] = {};
+  headers().forEach((value: any, key: any) => (headersForContext[key] = value));
   return (
     <html lang="en" className="dark">
-    <body className={cn(
-      'min-h-screen bg-background font-sans antialiased',
-      fontSans.variable,
-    )}>
-    {/* todo remove tma pink*/}
-    {/*<TMAProvider headers={headersForContext}>*/}
-    <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-      <div className="relative flex h-screen flex-col">
-        <main className="">
-          {children}
-        </main>
-        <Toaster />
-      </div>
-    </Providers>
-    {/*</TMAProvider>*/}
-    </body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        {/* todo remove tma pink*/}
+        {/*<TMAProvider headers={headersForContext}>*/}
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex h-screen flex-col">
+            <main className="">{children}</main>
+            <Toaster />
+          </div>
+        </Providers>
+        {/*</TMAProvider>*/}
+      </body>
     </html>
   );
 }
